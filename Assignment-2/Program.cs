73a16e4188464cfe41ef8c71cf5587a15a5def18
macparihar <156152583+macparihar@users.noreply.github.com>
2024-03-17 13:44:47 -0600

    done

---
+++ Assignment-2/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        for (int nums = 0; nums < 10; nums++)
+        {
+            if (nums % 2 == 0)
+            {
+                Console.WriteLine($"{nums} is an Even Number");
+                sumEven += nums;
+                //sumEven = sumEven + nums;
+            }
+            else
+            {
+                Console.WriteLine($"{nums} is an Odd Number");
+                sumOdd += nums;
+                //sumOdd = sumOdd + nums;
+            }
+        }
+
+        Console.WriteLine($"Total of even numbers: {sumEven}");
+        Console.WriteLine($"Total of odd numbers: {sumOdd}");
+
+
+        Console.ReadKey();
+    }
+}
